270ebb696608a436aab8acbab3bda26ced7f6504
AspieSoft <aspiesoftweb@gmail.com>
2023-03-14 22:50:03 -0400

    success

--- goque.go
+++ goque.go
@@ -4,109 +4,161 @@ import (
 	"time"
 )
 
-const queueSize uint16 = 65535
+// const queueSize uint16 = 65535
+const queueSize uint8 = 255
 
 type Queue[T any] struct {
-	start *uint16
-	end *uint16
+	start *uint8
+	end *uint8
 	size *uintptr
+	rmSize *uintptr
 
 	queue *[uint32(queueSize)+1]qVal[T]
 	overflow *[]qVal[T]
 	null T
 
-	// shiftOverflow chan bool
-	add chan qVal[T]
+	in chan qVal[T]
+	fixing *bool
 }
 
 type qVal[T any] struct {
 	mode uint8
 	val T
-	// hasVal bool
-	// overflow bool
 }
 
 func New[T any]() *Queue[T] {
-	start := uint16(0)
-	end := uint16(0)
+	start := uint8(0)
+	end := uint8(0)
 	size := uintptr(0)
+	rmSize := uintptr(0)
 
 	queue := [uint32(queueSize)+1]qVal[T]{}
 	overflow := []qVal[T]{}
 
-	add := make(chan qVal[T])
+	in := make(chan qVal[T])
+	fixing := false
 
-	q := &Queue[T]{
+	q := Queue[T]{
 		start: &start,
 		end: &end,
 		size: &size,
+		rmSize: &rmSize,
 
 		queue: &queue,
 		overflow: &overflow,
 
-		add: add,
+		in: in,
+		fixing: &fixing,
 	}
 
 	go func(){
 		for {
-			val := <-add
-			if val.mode == 1 {
+			inp := <-in
+			if inp.mode == 1 { // Add
 				if size > uintptr(queueSize) {
-					overflow = append(overflow, val)
-				}else{
-					queue[end] = val
+					overflow = append(overflow, inp)
 					size++
+				}else{
+					queue[end] = inp
 					end++
+					size++
 				}
-			}else if val.mode >= 2 {
-				if val.mode == 2 && size > 0 {
-					size--
-				}
-
-				if len(overflow) != 0 && size <= uintptr(queueSize) {
+			}else if inp.mode == 2 { // Next
+				if len(overflow) != 0 {
 					queue[end] = overflow[0]
 					overflow = overflow[1:]
 					end++
 				}
-			}else{
-				break
+
+				fixing = true
+				time.Sleep(10 * time.Nanosecond)
+				size--
+				rmSize--
+				fixing = false
 			}
 		}
 	}()
 
-	return q
+	return &q
+}
+
+// wait will try to wait and check again if the queue reports empty
+//
+// @return bool
+//
+// true = finished
+//
+// false = timeout
+func (q *Queue[T]) wait(value T) bool {
+	for *q.fixing {
+		time.Sleep(10 * time.Nanosecond)
+	}
+
+	if *q.size - *q.rmSize == 0 {
+		loops := 100000
+		for *q.fixing || (*q.size - *q.rmSize == 0 && *q.start == *q.end && loops > 0) {
+			if !*q.fixing {
+				loops--
+			}
+			time.Sleep(10 * time.Nanosecond)
+		}
+
+		if *q.size - *q.rmSize == 0 {
+			return false
+		}
+	}
+
+	return true
 }
 
 func (q *Queue[T]) Add(value T){
-	q.add <- qVal[T]{mode: 1, val: value}
+	q.in <- qVal[T]{1, value}
 }
 
 func (q *Queue[T]) Next() T {
-	if *q.size == 0 {
-		return q.null
+	// if empty, try to wait and check again
+	for *q.fixing {
+		time.Sleep(10 * time.Nanosecond)
 	}
 
-	val := q.queue[*q.start]
+	if *q.size - *q.rmSize == 0 {
+		loops := 100000
+		for *q.fixing || (*q.size - *q.rmSize == 0 && *q.start == *q.end && loops > 0) {
+			if !*q.fixing {
+				loops--
+			}
+			time.Sleep(10 * time.Nanosecond)
+		}
 
-	for val.mode == 0 {
-		q.add <- qVal[T]{mode: 3}
-		time.Sleep(10 * time.Nanosecond)
-		val = q.queue[*q.start]
-
-		if val.mode == 0 && *q.size == 0 {
+		if *q.size - *q.rmSize == 0 {
 			return q.null
 		}
 	}
 
+	val := q.queue[*q.start]
 	*q.start++
-	q.add <- qVal[T]{mode: 2}
+	*q.rmSize++
+
+	q.in <- qVal[T]{mode: 2}
+
 	return val.val
 }
 
-func (q *Queue[T]) Empty() bool {
-	return *q.size == 0
-}
+func (q *Queue[T]) Len() uintptr {
+	// if empty, try to wait and check again
+	for *q.fixing {
+		time.Sleep(10 * time.Nanosecond)
+	}
 
-func (q *Queue[T]) Stop() {
-	q.add <- qVal[T]{}
+	if *q.size - *q.rmSize == 0 {
+		loops := 100000
+		for *q.fixing || (*q.size - *q.rmSize == 0 && *q.start == *q.end && loops > 0) {
+			if !*q.fixing {
+				loops--
+			}
+			time.Sleep(10 * time.Nanosecond)
+		}
+	}
+
+	return *q.size - *q.rmSize
 }
--- goque_test.go
+++ goque_test.go
@@ -2,59 +2,27 @@ package goque
 
 import (
 	"testing"
-	"time"
 )
 
 func Test(t *testing.T){
 	myq := New[int]()
 
-	// testSize := int(queueSize)*100
-	// testSize := int(queueSize)*10
-	testSize := int(queueSize)*3
-	// testSize := int(queueSize)-100
-	// testSize := 100
+	testSize := int(queueSize)*10
+	// testSize := int(queueSize)*3
+	// testSize := int(queueSize)+1
 
-
-	// done := false
 	go func(){
 		for i := 0; i < testSize; i++ {
-			myq.Add(i+1)
-			time.Sleep(10 * time.Nanosecond)
+			myq.Add(i)
 		}
-		// done = true
 	}()
 
-	//todo: issue only seems to affect concurrent reads with writes, and not concurrent writes directly
-	// maybe the overflow could be the issue
-
-	/* for !done {
-		time.Sleep(100 * time.Millisecond)
-	} */
-
-	// time.Sleep(100 * time.Nanosecond)
-	time.Sleep(100 * time.Millisecond)
-
-
-	res := []int{}
 	for i := 0; i < testSize; i++ {
 		v := myq.Next()
-		res = append(res, v)
-		time.Sleep(10 * time.Nanosecond)
-	}
 
-	time.Sleep(100 * time.Millisecond)
-
-	// fmt.Println(myq.overflow)
-
-	if len(res) != testSize {
-		t.Error("result did not match expected length\nLength", "\nexp:", testSize, "\ngot:", len(res))
-		return
-	}
-
-	for i := 0; i < len(res); i++ {
-		if res[i] != i+1 {
-			t.Error("result did not match expected output\nOutput", "\nexp:", i+1, "\ngot:", res[i])
-			return
+		if v != i {
+			t.Error("invalid value:", v, "!=", i)
+			break
 		}
 	}
 }
